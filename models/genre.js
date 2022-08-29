bb9a4340b186f29d327ba0a33cfcad01c5d26945
vvarhand <brechko.mv@gmail.com>
2022-08-29 19:41:49 +0200

    Main Schemas complete

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: { type: String, required: true, min: 3, max: 100 },
+  fiction: { type: Boolean, required: true },
+});
+
+GenreSchema.virtual("url").get(function () {
+  return `/catalog/genre/${this._id}`;
+});
+
+module.exports = mongoose.model("Genre", GenreSchema);

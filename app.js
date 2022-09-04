a8f1b21ecb68f90a55c53f4b87a42ae4e6ced949
vvarhand <brechko.mv@gmail.com>
2022-09-04 11:07:42 +0200

    Create Router Catalog

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+const wiki = require("./routes/wiki.js");
 
-const mongoDB = "mongodb+srv://mykocaine:fukkkpasswordtesting1@cluster0.md4vpkj.mongodb.net/test";
+const mongoDB =
+  "mongodb+srv://mykocaine:fukkkpasswordtesting1@cluster0.md4vpkj.mongodb.net/test";
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
@@ -28,6 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/wiki", wiki);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- models/author.js
+++ models/author.js
@@ -9,14 +9,13 @@ const AuthorSchema = new Schema({
   date_of_death: { type: Date },
 });
 
+// Virtual functions help us to create a URL for a specific author
 AuthorSchema.virtual("name").get(function () {
-  let fullname = "";
-  if (this.first_name && this.family_name) {
-    fullname = `${this.family_name}, ${this.first_name}`;
-  }
-  if (!this.first_name || !this.family_name) {
-    fullname = "";
-  }
+  let fullname =
+    this.first_name && this.family_name
+      ? `${this.first_name}, ${this.family_name}`
+      : "";
+
   return fullname;
 });
 
@@ -25,3 +24,14 @@ AuthorSchema.virtual("url").get(function () {
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
+
+/*
+
+if (this.first_name && this.family_name) {
+  fullname = `${this.family_name}, ${this.first_name}`;
+}
+if (!this.first_name || !this.family_name) {
+  fullname = "";
+}
+return fullname;
+*/

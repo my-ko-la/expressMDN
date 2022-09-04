a35e29d1d138c180e89b0d922dc6e60b49dd1a06
vvarhand <brechko.mv@gmail.com>
2022-09-04 12:21:57 +0200

    Creating templates with Pug

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require("morgan");
 const mongoose = require("mongoose");
 const wiki = require("./routes/wiki.js");
 
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
+
 const mongoDB =
   "mongodb+srv://mykocaine:fukkkpasswordtesting1@cluster0.md4vpkj.mongodb.net/test";
 
@@ -13,14 +17,11 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/wiki", wiki);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -15,7 +15,7 @@ const BookInstanceSchema = new Schema({
 });
 
 BookInstanceSchema.virtual("url").get(function () {
-  return `/catalog/bookinstance/${this._id}`;
+  return `/catalog/bookinstance/${this._id}`; //_id given by MongoDB
 });
 
 //Export model

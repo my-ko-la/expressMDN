83f10a2e14c11ced36f82463ede08f5dee7001fd
vvarhand <brechko.mv@gmail.com>
2022-09-05 16:09:24 +0200

    GET and POST for Genre delete

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -104,12 +104,67 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+  async.parallel(
+    {
+      genre(callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+      genres_books(callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        res.redirect("/catalog/genres");
+      }
+      res.render("genre_delete", {
+        title: "Delete Genre",
+        genre: results.genre,
+        genres_books: results.genres_books,
+      });
+    }
+  );
 };
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.body.genreid).exec(callback);
+      },
+      genres_books: function (callback) {
+        Book.find({ genre: req.body.genreid }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      // Success
+      if (results.genres_books.length > 0) {
+        // Genre has books. Render in same way as for GET route.
+        res.render("genre_delete", {
+          title: "Delete Genre",
+          genre: results.genre,
+          genres_books: results.genres_books,
+        });
+        return;
+      } else {
+        // Genre has no books. Delete object and redirect to the landing page
+        Genre.findByIdAndRemove(req.body.genreid, function deleteGenre(err) {
+          if (err) {
+            return next(err);
+          }
+          // Success - go to genre list
+          res.redirect("/catalog/genres");
+        });
+      }
+    }
+  );
 };
 
 // Display Genre update form on GET.
--- models/author.js
+++ models/author.js
@@ -40,14 +40,3 @@ AuthorSchema.virtual("lifespan").get(function () {
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
-
-/*
-
-if (this.first_name && this.family_name) {
-  fullname = `${this.family_name}, ${this.first_name}`;
-}
-if (!this.first_name || !this.family_name) {
-  fullname = "";
-}
-return fullname;
-*/
